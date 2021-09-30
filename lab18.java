41abf4bc01b5e49773658f94004bb561591a1042
sravani-ca2 <89564826+sravani-ca2@users.noreply.github.com>
2021-09-30 22:23:51 +0530

    Add files via upload

---
+++ lab18.java
@@ -0,0 +1,19 @@
+import pack.*;
+public class sum
+{
+int a=20;
+int b=30;
+public void cal()
+{
+int s=a+b;
+System.out.println("sum:"+s);
+}
+}
+class sum Test
+{
+public static void main(String[] args)
+{
+sum s=new sum();
+s.cal();
+}
+} 
